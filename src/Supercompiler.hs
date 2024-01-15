dea68a96ae3d7d6429ab08bb15222f1308470cfa
SophieBosio <sophie.bosio@outlook.com>
2024-01-15 11:31:12 +0100

    Added major function type signatures

--- src/Supercompiler.hs
+++ src/Supercompiler.hs
@@ -12,6 +12,12 @@ type Var       = Term Type
 type FreeVars  = [Var]
 
 type Heap = Map Var (Term Type)
+type Stack     = [StackFrame]
+type TermState = (Heap, Term Type, Stack)
+type History   = [TermState]
+
+data TermResult = Stop | Continue History
+
 data StackFrame = StackFrame
   { update      :: Var
   , apply       :: Term Type
@@ -19,9 +25,6 @@ data StackFrame = StackFrame
   , applyFirst  :: Term Type
   , applySecond :: Term Type
   }
-type Stack     = [StackFrame]
-type TermState = (Heap, Term Type, Stack)
-type History   = [TermState]
 
 data Promise = Promise
   { name    :: Var
@@ -43,3 +46,41 @@ type Supercompiler a = State SupercompilerState a
 supercompile :: Term Type -> Term Type
 supercompile = undefined
 
+sc :: History -> TermState -> Supercompiler (Term Type)
+sc = undefined
+
+
+-- Monad operations
+freshName :: Supercompiler Var
+freshName = undefined
+
+promise :: Promise -> Supercompiler ()
+promise = undefined
+
+bind :: Var -> Term Type -> Supercompiler ()
+bind = undefined
+
+memo :: (TermState -> Supercompiler (Term Type)
+      -> TermState -> Supercompiler (Term Type))
+memo = undefined
+
+match :: TermState -> TermState -> Maybe (Var -> Var)
+match = undefined
+
+
+-- Evaluator
+reduce :: TermState -> TermState
+reduce = undefined
+
+
+-- Splitter
+split :: Monad m => (TermState -> m (Term Type)) -> TermState -> m (Term Type)
+split = undefined
+
+
+-- Termination checking
+emptyHistory :: History
+emptyHistory = []
+
+terminate :: History -> TermState -> TermResult
+terminate = undefined
