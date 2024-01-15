95b6b6e95969ed58b7d5cc39de4b1f189017e327
SophieBosio <sophie.bosio@outlook.com>
2024-01-15 14:00:00 +0100

    Renaming, util functions, and refactoring

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -48,61 +48,81 @@ evaluate (Pattern (Constructor c ps a)) =
 --         else do let u = fst $ head us -- take the first possible unifier
 --                 let t = snd $ head us -- take the corresponding branch
 --                 evaluate $ substituteWithUnifier u t
--- evaluate (Fst p _) =
---   do ts <- evaluate p >>= pair
---      return $ fst ts
--- evaluate (Snd p _) =
---   do ts <- evaluate p >>= pair
---      return $ snd ts
--- evaluate (Plus t1 t2 a) =
---   do m <- evaluate t1 >>= number
---      n <- evaluate t2 >>= number
---      return $ Number (m + n) a
--- evaluate (Minus t1 t2 a) =
---   do m <- evaluate t1 >>= number
---      n <- evaluate t2 >>= number
---      return $ Number (m - n) a
--- evaluate (Lt    t0 t1 a) =
---   do m <- evaluate t0 >>= number
---      n <- evaluate t1 >>= number
---      return $ Boolean (m < n) a
--- evaluate (Gt    t0 t1 a) =
---   do m <- evaluate t0 >>= number
---      n <- evaluate t1 >>= number
---      return $ Boolean (m > n) a
--- evaluate (Equal t0 t1 a) =
---   do m <- evaluate t0 >>= number
---      n <- evaluate t1 >>= number
---      return $ Boolean (m == n) a
--- evaluate (Not t0 a) =
---   do b <- evaluate t0 >>= bool
---      return $ Boolean (not b) a
--- evaluate _ = error "expected a non-canonical term!"
-
-
+evaluate (Fst p _) =
+  do ts <- evaluate p >>= pair
+     return $ fst ts
+evaluate (Snd p _) =
+  do ts <- evaluate p >>= pair
+     return $ snd ts
+evaluate (Plus t1 t2 a) =
+  do m <- evaluate t1 >>= number
+     n <- evaluate t2 >>= number
+     return $ Pattern $ Number (m + n) a
+evaluate (Minus t1 t2 a) =
+  do m <- evaluate t1 >>= number
+     n <- evaluate t2 >>= number
+     return $ Pattern $ Number (m - n) a
+evaluate (Lt    t0 t1 a) =
+  do m <- evaluate t0 >>= number
+     n <- evaluate t1 >>= number
+     return $ Pattern $ Boolean (m < n) a
+evaluate (Gt    t0 t1 a) =
+  do m <- evaluate t0 >>= number
+     n <- evaluate t1 >>= number
+     return $ Pattern $ Boolean (m > n) a
+evaluate (Equal t0 t1 a) =
+  do m <- evaluate t0 >>= number
+     n <- evaluate t1 >>= number
+     return $ Pattern $ Boolean (m == n) a
+evaluate (Not t0 a) =
+  do b <- evaluate t0 >>= bool
+     return $ Pattern $ Boolean (not b) a
+evaluate _ = error "expected a non-canonical term!"
 
 
 -- Substitution
 substitute :: X -> Term a -> (Term a -> Term a)
-substitute x t v = -- computes t[v/x].
+substitute x t v = -- computes t[v/x]
   case t of
     Pattern (Variable  y  _) | x == y -> v
-    Pattern (Constructor c ps a)      ->
-      Pattern (Constructor c (map (manipulateWith f) ps) a)
-    Pattern (Pair  t1 t2 a) -> Pattern (Pair  (f t1) (f t2)                 a)
-    Lambda  y t1 a | x /= y -> Lambda y (f t1)                              a
-    Application  t1 t2 a    -> Application (f t1) (f t2)                    a
-    Let  y t1 t2 a          -> Let y (f t1) ((if x == y then id else f) t2) a
-    Rec  y t1    a | x /= y -> Rec y (f t1)                                 a
-    Fst   t0     a          -> Fst   (f t0)                                 a
-    Snd   t0     a          -> Snd   (f t0)                                 a
-    Plus  t0 t1  a          -> Plus  (f t0) (f t1)                          a
-    Minus t0 t1  a          -> Minus (f t0) (f t1)                          a
-    Lt    t0 t1  a          -> Lt    (f t0) (f t1)                          a
-    Gt    t0 t1  a          -> Gt    (f t0) (f t1)                          a
-    Equal t0 t1  a          -> Equal (f t0) (f t1)                          a
-    Not   t0     a          -> Not   (f t0)                                 a
+    Pattern (Constructor c ps a) ->
+      Pattern (Constructor c (map (manipulateWith subs) ps) a)
+    Pattern (Pair  t1 t2 a) ->
+      Pattern (Pair  (subs t1) (subs t2) a)
+    Lambda  y t1 a | x /= y -> Lambda y (subs t1)              a
+    Application  t1 t2 a    -> Application (subs t1) (subs t2) a
+    Let  y t1 t2 a          ->
+      Let y (subs t1) ((if x == y then id else subs) t2) a
+    Rec  y t1    a | x /= y -> Rec y (subs t1)                 a
+    Fst   t0     a          -> Fst   (subs t0)                 a
+    Snd   t0     a          -> Snd   (subs t0)                 a
+    Plus  t0 t1  a          -> Plus  (subs t0) (subs t1)       a
+    Minus t0 t1  a          -> Minus (subs t0) (subs t1)       a
+    Lt    t0 t1  a          -> Lt    (subs t0) (subs t1)       a
+    Gt    t0 t1  a          -> Gt    (subs t0) (subs t1)       a
+    Equal t0 t1  a          -> Equal (subs t0) (subs t1)       a
+    Not   t0     a          -> Not   (subs t0)                 a
     _                         -> t
   where
-    f = flip (substitute x) v
+    subs = flip (substitute x) v
     manipulateWith f = strengthenToPattern . f . weakenToTerm
+
+
+-- Utility functions
+bool :: Show a => Term a -> Runtime a Bool
+bool (Pattern (Boolean b _)) = return b
+bool t = error $ "expected a boolean value, but got a " ++ show t
+
+number :: Show a => Term a -> Runtime a Integer
+number (Pattern (Number n _)) = return n
+number t = error $ "expected an integer, but got a " ++ show t
+
+pair :: Show a => Term a -> Runtime a (Term a, Term a)
+pair (Pattern (Pair t1 t2 _)) = return (t1, t2)
+pair t = error $ "expected a pair, but got a " ++ show t
+
+-- function :: Show a => Term a -> Runtime a (Term a -> Term a)
+-- function (Lambda x t a) =
+--   do notAtTopLevel (x, a)
+--      return $ substitute x t
+-- function t = error $ "expected a function, but got a " ++ show t
