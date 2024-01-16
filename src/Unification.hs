aa46ab976a489d4cd027830dafb9ab578c356029
SophieBosio <sophie.bosio@outlook.com>
2024-01-16 11:51:41 +0100

    unify and unify'

--- src/Unification.hs
+++ src/Unification.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module Unification where
 
 import Syntax
@@ -11,48 +13,33 @@ data PatternMatch a =
     NoMatch
   | MatchBy (Transformation Pattern a)
 
-errorMessage :: Term a -> Pattern a -> b
-errorMessage p q = error $
-  "couldn't match " ++ show p ++ " against pattern " ++ show q
-
 type Unifier a = Maybe (a -> a)
 
 newtype Substitution meta a = Substitution { unifier :: Unifier (meta a) }
 
 
 -- Export
-patternMatch :: Term a -> Pattern a -> PatternMatch a
+patternMatch :: Term a -> Term a -> PatternMatch a
 patternMatch p q = maybe NoMatch MatchBy (unifier $ unify p q)
 
 
 -- Unification
-unify :: Term a -> Pattern a -> Substitution Pattern a
-unify (Pattern p1) = unify' p1
--- unify Rec         X      (T0 a)            a
--- unify Let         X      (T1 a) (T2 a)     a
--- unify Application (T1 a) (T2 a)            a
--- unify Case        (T0 a) [(Alt a, Body a)] a
---   -- Utilities:
--- unify Fst         (T0 a)                   a
--- unify Snd         (T0 a)                   a
--- unify Plus        (T0 a) (T1 a)            a
--- unify Minus       (T0 a) (T1 a)            a 
--- unify Lt          (T0 a) (T1 a)            a
--- unify Gt          (T0 a) (T1 a)            a
--- unify Equal       (T0 a) (T1 a)            a
--- unify Not         (T0 a)                   a
+unify :: Term a -> Term a -> Substitution Pattern a
+unify (Pattern p) (Pattern q) = unify' p q
+unify _            _          = Substitution Nothing
 
 unify' :: Pattern a -> Pattern a -> Substitution Pattern a
 unify' (Variable x _) (Variable y _) | x == y = mempty
 unify' (Variable x _) p              | not $ p `contains` x = p `substitutes` x
 unify' p              (Variable x _) | not $ p `contains` x = p `substitutes` x
--- unify' (Constructor c ps _) (Constructor c' ps' _)
---   | c == c' && length ps == length ps'
---   = 
--- unify' (Unit _)
--- unify' (Number n _)
--- unify' (Boolean b _)
--- unify' (Pair t0 t1)
+unify' (Unit       _) (Unit         _) = mempty
+unify' (Number   n _) (Number     m _) | n == m = mempty
+unify' (Boolean  b _) (Boolean    c _) | b == c = mempty
+unify' (Pair t0 t1 _) (Pair t0' t1' _) = unify t0 t0' `mappend` unify t1 t1'
+unify' (Constructor c ps _) (Constructor c' ps' _)
+  | c == c' && length ps == length ps'
+  = foldr (mappend . uncurry unify') mempty (zip ps ps')
+unify' _             _                 = Substitution Nothing
 
 
 -- Substitution
@@ -74,3 +61,6 @@ isMatch _       = True
 
 contains :: Pattern a -> X -> Bool
 contains = undefined
+
+-- mapsTo :: X -> Pattern a -> Transformation Pattern a
+-- mapsTo (Variable x _) y | x == y = 
