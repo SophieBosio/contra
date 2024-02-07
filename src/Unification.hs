3aa800bc9f3c2985298d0302e323b997e42b1610
SophieBosio <sophie.bosio@outlook.com>
2024-02-07 14:13:46 +0100

    Unification algo with Value

--- src/Unification.hs
+++ src/Unification.hs
@@ -23,20 +23,29 @@ patternMatch p q = maybe NoMatch MatchBy (unifier $ unify p q)
 -- Unification
 unify :: Term a -> Term a -> Substitution Pattern a
 unify (Pattern p) (Pattern q) = unify' p q
-unify _            _          = Substitution Nothing -- Only patterns can match patterns
+unify (TConstructor c ts a) (TConstructor c' ts' a')
+  | all canonical ts && all canonical ts'
+  = unify' (PConstructor c  (map strengthenToPattern ts)  a)
+           (PConstructor c' (map strengthenToPattern ts') a')
+unify _ _ = Substitution Nothing -- Only patterns can match patterns
 
 unify' :: Pattern a -> Pattern a -> Substitution Pattern a
+unify' (Value      v) (Value      w) = unify'' v w
 unify' (Variable x _) (Variable y _) | x == y = mempty
 unify' (Variable x _) p              | not $ p `contains` x = p `substitutes` x
 unify' p              (Variable x _) | not $ p `contains` x = p `substitutes` x
-unify' (Unit       _) (Unit         _) = mempty
-unify' (Number   n _) (Number     m _) | n == m = mempty
-unify' (Boolean  b _) (Boolean    c _) | b == c = mempty
-unify' (Constructor c ps _) (Constructor c' ps' _)
+unify' (PConstructor c ps _) (PConstructor c' ps' _)
   | c == c' && length ps == length ps'
   = foldr (mappend . uncurry unify') mempty (zip ps ps')
 unify' _             _                 = Substitution Nothing
 
+unify'' :: Value a -> Value a -> Substitution Pattern a
+unify'' (Unit       _) (Unit         _) = mempty
+unify'' (Number   n _) (Number     m _) | n == m = mempty
+unify'' (Boolean  b _) (Boolean    c _) | b == c = mempty
+unify'' (VConstructor c vs _) (VConstructor c' vs' _)
+  | c == c' && length vs == length vs'
+  = foldr (mappend . uncurry unify'') mempty (zip vs vs')
 
 -- Substitution
 instance Semigroup (Substitution meta a) where
@@ -53,7 +62,7 @@ substitutes p x = Substitution $ return $ x `mapsTo` p
 -- Utility functions
 contains :: Pattern a -> X -> Bool
 contains (Variable        x _) y | x == y = True
-contains (Constructor  _ ps _) y = any (`contains` y) ps
+contains (PConstructor _ ps _) y = any (`contains` y) ps
 contains _                     _ = False
 
 mapsTo :: X -> Pattern a -> Transformation Pattern a
