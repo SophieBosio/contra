9675296aa8dd58f0de4241c0b8280dcca4d7d6a0
SophieBosio <sophie.bosio@outlook.com>
2024-02-26 16:16:02 +0100

    WIP Constraints/Property Engine setup

--- src/Constrainer.hs
+++ src/Constrainer.hs
@@ -9,22 +9,31 @@ import Control.Monad.RWS
 -- Abbreviations
 type Unknown = Term Type
 
-data Constraint = Unknown :=: Unknown
+data Equality = Unknown :=: Unknown
   deriving Show
 
-type Mapping a b = a -> b
-type MapsTo  a b = Mapping a b -> Mapping a b
-type Environment = Mapping Name Unknown
+type Mapping   a b = a -> b
+type MapsTo    a b = Mapping a b -> Mapping a b
+type Constraint    = Mapping Name Range
+type Instantiation = [(Name, Value Type)]
 
 data Range =
     Undefined Type
   | Fixed
   | Unknown Unknown
 
+type Constrainer = RWS Constraint [Equality] Index
 
--- Constrainer Monad
-type Constrainer = RWS Environment [Constraint] Index
 
+-- Export
+constraints :: Program Type -> (P, Term Type) -> Constraint
+constraints program = resolve generatedConstraints
+  where
+    --  r w s a -> (a, s, w)
+    (_, _, generatedConstraints) = runRWS (constrainProgram program) emptyEnvironment 0
+
+
+-- Setup
 fresh :: Constrainer Unknown
 fresh =
   do i <- get      -- Get state
@@ -40,11 +49,27 @@ bind x a look y = if x == y
 equalTo :: Unknown -> Unknown -> Constrainer ()
 u1 `equalTo` u2 = tell [u1 :=: u2]
 
+emptyEnvironment :: Constraint
+emptyEnvironment = error . (++ " is unbound!")
+
 
 -- Main functions
+constrainProgram :: Program Type -> Constrainer (Program Type)
+constrainProgram = undefined
+
+constrain :: Unknown -> Constrainer Unknown
+constrain = undefined
+-- constrain :: Program Type -> (Term Type -> Term Type)
+-- constrain program property = 
+
+
 -- equalise :: Term Type -> Constrainer Unknown
+-- equalise = undefined
 -- equalise (Pattern p) = equalise' p
 -- -- equalise (Lambda x t0 _)
 
 -- equalise' :: Pattern a -> Constrainer Unknown
 -- equalise' = undefined
+
+resolve :: [Equality] -> (P, Term Type) -> Constraint
+resolve = undefined
--- src/PropertyEngine.hs
+++ src/PropertyEngine.hs
@@ -6,11 +6,13 @@ import Syntax
 import PartialEvaluator (partiallyEvaluate)
 
 import Control.Monad    (foldM_)
+import Control.Monad.Reader
 import Data.SBV
 
 
--- Abbrevations
-data Prover a = Prover {}
+-- Abbreviations
+type Input   = Term Type
+type Formula = Reader Input
 
 
 -- Export
@@ -24,9 +26,13 @@ check program =
 checkProp :: Program Type -> (P, Term Type) -> IO (Program Type)
 checkProp prog (propName, prop) =
   do putStr $ "Testing " ++ propName ++ " ❯ "
+     -- Inline function definitions used in the property
      let (prop', residual) = partiallyEvaluate prog prop
-     let f = generateFormula residual prop'
+     -- Generate formula from term
+     let f = generateFormula prop'
+     -- Prove formula
      proveFormula f
+     -- Return residual program for memoisation
      return residual
 
 
@@ -34,15 +40,34 @@ checkProp prog (propName, prop) =
 proveFormula :: Provable a => a -> IO ()
 proveFormula f =
   do result <- prove f
-     if modelExists result
+     if not (modelExists result)
        then putStrLnGreen  " ✓ OK "
        else do putStrLnRed " ✱ FAIL "
-               putStrLn "Counterexample: "
+               putStrLn "\tCounterexample: "
+               putStr "\t"
                print result -- TODO: Translate back into pretty terms
 
-generateFormula :: Program Type -> Term Type -> SBool
-generateFormula program property = undefined
+generateFormula :: Term Type -> SBool
+generateFormula property = runReader (translate property) property
   -- do cs <- constraints program property
+
+
+-- Constraint generation
+translate :: Term Type -> Formula SBool
+translate (Equal t0 t1 _) =
+  do t0' <- translate t0
+     t1' <- translate t1
+     return $ t0' .== t1'
+translate (Lt t0 t1 _) =
+  do t0' <- translate t0
+     t1' <- translate t1
+     return $ t0' .< t1'
+translate (Gt t0 t1 _) =
+  do t0' <- translate t0
+     t1' <- translate t1
+     return $ t0' .> t1'
+-- translate (Lambda x t0 a) =  
+-- translate (Application t1 t2 _) =
 
 
 -- Utility
