d9fd05befa6f22cfb9e46bedc4762782e5ed4a28
SophieBosio <sophie.bosio@outlook.com>
2024-03-14 17:46:06 +0100

    Simple property checker tests

--- test/PropertyCheckerTests.hs
+++ test/PropertyCheckerTests.hs
@@ -11,7 +11,10 @@ import Data.SBV
 
 -- Export: Test groups
 simple :: TestTree
-simple = undefined
+simple =
+  testGroup "Checking simple properties: " $
+       simpleSatisfiableProps
+    ++ simpleUnsatisfiableProps
 
 
 -- Helpers
@@ -33,4 +36,62 @@ unsatisfiable prop =
 
 
 -- Tests
+simpleSatisfiableProps :: [TestTree]
+simpleSatisfiableProps =
+  map (\p -> testCase ("Checking property '" ++ show p ++ "'")
+             (satisfiable p))
+    -- commutativity
+  [ Lambda (Variable "x" Integer')
+    (Lambda (Variable "y" Integer')
+      (Equal
+        (Plus
+          (Pattern (Variable "x" Integer'))
+          (Pattern (Variable "y" Integer'))
+          Integer')
+        (Plus
+          (Pattern (Variable "y" Integer'))
+          (Pattern (Variable "x" Integer'))
+          Integer')
+        Boolean')
+      (Integer' :->: Boolean'))
+    (Integer' :->: (Integer' :->: Boolean'))
+    -- associativity
+  , Lambda (Variable "x" Integer')
+    (Lambda (Variable "y" Integer')
+      (Lambda (Variable "z" Integer')
+        (Equal
+          (Plus
+            (Plus
+              (Pattern (Variable "x" Integer'))
+              (Pattern (Variable "y" Integer')) Integer')
+            (Pattern (Variable "z" Integer')) Integer')
+          (Plus
+            (Pattern (Variable "x" Integer'))
+            (Plus
+              (Pattern (Variable "y" Integer'))
+              (Pattern (Variable "z" Integer')) Integer')
+            Integer')
+          Boolean')
+        (Integer' :->: Boolean'))
+      (Integer' :->: (Integer' :->: Boolean')))
+    (Integer' :->: (Integer' :->: (Integer' :->: Boolean')))
+    -- right identity
+  , Lambda (Variable "x" Integer')
+    (Equal
+      (Plus
+        (Pattern (Variable "x" Integer'))
+        (Pattern (Value (Number 0 Integer'))) Integer')
+      (Pattern (Variable "x" Integer')) Boolean')
+    (Integer' :->: Boolean')
+    -- left identity
+  , Lambda (Variable "x" Integer')
+    (Equal
+      (Plus
+        (Pattern (Value (Number 0Integer')))
+        (Pattern (Variable "x" Integer')) Integer')
+      (Pattern (Variable "x" Integer')) Boolean')
+    (Integer' :->: Boolean')
+  ]
 
+simpleUnsatisfiableProps :: [TestTree]
+simpleUnsatisfiableProps = []
