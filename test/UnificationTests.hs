9d4358cc93521e5d41c0aafa0841140a8b5a31e3
SophieBosio <sophie.bosio@outlook.com>
2024-05-13 14:59:51 +0200

    Add some more unification tests

--- test/UnificationTests.hs
+++ test/UnificationTests.hs
@@ -1,7 +1,7 @@
 module UnificationTests where
 
 import Core.Syntax
-import Analysis.Unifier
+import Analysis.Unification
 
 import Test.Tasty
 import Test.Tasty.HUnit
@@ -18,6 +18,7 @@ substitutionTests =
 computeSubstitutionOK :: (Show a, Eq a) => Pattern a -> Term a
                                         -> Term a -> Term a -> Assertion
 computeSubstitutionOK x t v e =
+  -- Computing 'x' in 't' instead of 'v' should give 'e'
   assertEqual "Should be equal after substitution."
     e (substitute x t v)
 
@@ -41,4 +42,20 @@ substituteVars =
     , Plus (Pattern (Variable "x" ())) (Pattern (Variable "y" ())) ()
     , TConstructor "Ctr" [Pattern (Value (Number 5 ())), Pattern (Value (Number 3 ()))] ()
     , Plus (Pattern (Value (Number 5 ()))) (Pattern (Value (Number 3 ()))) ())
+  ]
+
+substituteADTs :: [TestTree]
+substituteADTs =
+  map (\(x, t, v, e) -> testCase
+                        ("(" ++ show t ++ ")[" ++ show v ++ "/" ++ show x ++ "]" ++
+                         " should give " ++ show e) $
+                        computeSubstitutionOK x t v e)
+  [ ( Variable "x" ()
+    , Pattern (PConstructor "C" [PConstructor "D" [PConstructor "E" [Variable "x" ()] ()] ()] ())
+    , Pattern (Value (Number 5 ()))
+    , Pattern (PConstructor "C" [PConstructor "D" [PConstructor "E" [Value (Number 5 ())] ()] ()] ()))
+  , ( Variable "x" ()
+    , Pattern (PConstructor "C" [PConstructor "D" [PConstructor "E" [Variable "x" ()] ()] ()] ())
+    , Pattern (Value (VConstructor "F" [Number 10 (), Boolean True ()] ()))
+    , Pattern (PConstructor "C" [PConstructor "D" [PConstructor "E" [Value (VConstructor "F" [Number 10 (), Boolean True ()] ())] ()] ()] ()))
   ]
