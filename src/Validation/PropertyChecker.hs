de15d4c36131b223e6bc85d18368bca52cace2a2
SophieBosio <sophie.bosio@outlook.com>
2024-05-14 17:01:33 +0200

    Sketch for pretty printing

--- src/Validation/PropertyChecker.hs
+++ src/Validation/PropertyChecker.hs
@@ -49,8 +49,14 @@ import Environment.ERSymbolic
 import Validation.Formula
 import Validation.Translator
 
-import Control.Monad (foldM_)
+import Control.Monad                (foldM_)
+import Data.Maybe                   (fromMaybe)
+import Data.List                    (elem, isPrefixOf, isSuffixOf,
+                                     intercalate, stripPrefix, sortBy)
+import Data.Ord                     (comparing)
+import qualified Data.Map.Strict as Map
 import Data.SBV
+import Data.SBV.Internals           (CV)
 
 
 -- * Abbreviations
@@ -59,33 +65,38 @@ type Property    = Term Type
 
 
 -- * Export
-check :: RecursionDepth -> Program Type -> IO ()
-check depth program =
+check :: Bool -> RecursionDepth -> Program Type -> IO ()
+check pretty depth program =
   -- For each property, collect the residual program
   -- and check next property with already specialised program
-  foldM_ (checkProperty depth) program (properties program)
+  foldM_ (checkProperty pretty depth) program (properties program)
 
 
-checkProperty :: RecursionDepth -> Program Type -> PropertyDef -> IO (Program Type)
-checkProperty depth prog (propName, prop) =
-  do putStr $ "Checking '" ++ propName ++ "' ❯ "
+checkProperty :: Bool -> RecursionDepth -> Program Type -> PropertyDef -> IO (Program Type)
+checkProperty pretty depth prog (propName, prop) =
+  do let symbol = if pretty then " ⇒ " else " >>> "
+     putStr $ "Checking '" ++ propName ++ "'" ++ symbol
      let (prop', residual) = partiallyEvaluate prog prop
-     let f = generateFormula depth residual prop' 
-     proveFormula f
+     let f = generateFormula depth residual prop'
+     let reconstructor = indexReconstruct prog
+     proveFormula pretty f reconstructor
      return residual
 
 
 -- * Main functions
-proveFormula :: Symbolic SBool -> IO ()
-proveFormula f =
-  do r@(ThmResult result) <- prove f
+proveFormula :: Bool -> Symbolic SBool -> ((D, Integer) -> (D, C)) -> IO ()
+proveFormula pretty f reconstructor =
+  do let ok      = if pretty then " ✓" else ""
+     let failed  = if pretty then " ✱" else ""
+     let unknown = if pretty then " ●" else ""
+     r@(ThmResult result) <- prove f
      case result of
-       Unsatisfiable _ _ -> putStrLnGreen  " ✓ OK "
-       Satisfiable   _ _ -> do putStrLnRed " ✱ FAIL "
-                               putStr " "
+       Unsatisfiable _ _ -> putStrLnGreen  $ ok     ++ " OK "
+       Satisfiable   _ _ -> do putStrLnRed $ failed ++ " FAIL "
                                print r
-                               -- TODO: printCounterExample m
-       _                 -> do putStrLnYellow " ● Unknown result: "
+                               -- printCounterExample reconstructor $
+                               --   getModelDictionary result
+       _                 -> do putStrLnYellow $ unknown ++ " Unknown result: "
                                print r
 
 generateFormula :: RecursionDepth -> Program Type -> Property -> Symbolic SBool
@@ -103,4 +114,91 @@ realise sv =
     other        -> error $
                     "Unexpected error: Property should translate to a \
                     \Boolean formula, but was a " ++ show other
+
+
+-- * Pretty-printing
+-- printCounterExample :: ((D, Integer) -> (D, C)) -> Map.Map String CV -> IO ()
+-- printCounterExample reconstructor m =
+--   do putStrLn "Counterexample:"
+--      putStrLn $ prettyPrint reconstructor (Map.toList m)
+--      putStrLn " "
+
+-- prettyPrint :: ((D, Integer) -> (D, C)) -> [(Name, CV)] -> String
+-- prettyPrint reconstructor assignments
+--   | null assignments  = "*** There are no variables bound by the model. ***"
+--   | null relevantVars = "*** There are no model-variables bound by the model. ***"
+--   | otherwise         = display $ parsePretty reconstructor relevantVars
+--   where
+--     relevantVars = sortBy (comparing fst) $ filter (not . ignore . fst) assignments
+--     ignore var  = "__internal_sbv" `isPrefixOf` var || '_' `elem` var
+
+-- display :: [(Name, String)] -> String
+-- display svs = intercalate "\n" $ map line svs
+--   where
+--     line (var, sv) = "  "
+--                       ++ justifyRight (longestName - length var) var
+--                       ++ " = "
+--                       ++ justifyLeft (longestVal - length sv) sv
+--     longestName = maximum $ 0 : [ length n  | (n, _ ) <- svs ]
+--     longestVal  = maximum $ 0 : [ length sv | (_, sv) <- svs ]
+--     justifyLeft  n s = s ++ replicate (n - length s) ' '
+--     justifyRight n s = replicate (n - length s) ' ' ++ s
+
+-- parsePretty :: ((D, Integer) -> (D, C)) -> [(Name, CV)] -> [(Name, String)]
+-- parsePretty _ [] = []
+-- parsePretty reconstructor ((var, cv) : rest)
+--   | '$' `elem` var = let x         = takeWhile (/= '$') var                     in
+--                      let pretty    = prettyADT reconstructor ((var, cv) : rest) in
+--                      let remaining = filter ((not . isPrefixOf var) . fst) rest in
+--                           (x, pretty) : parsePretty reconstructor remaining
+--   | otherwise       = (var, show cv) : parsePretty reconstructor rest
+
+-- splitAtChar :: Char -> String -> (String, String)
+-- splitAtChar char str =
+--   let (before, after) = break (== char) str
+--   in  (before, drop 1 after)
+
+
+-- prettyADT :: ((D, Integer) -> (D, C)) -> [(Name, CV)] -> String
+-- prettyADT _ [] = ""
+-- prettyADT reconstructor svs@((adtVar, _) : _) =
+--   let (x, adt)  = splitAtChar '$' adtVar                in
+--   let ctrs      = prettyConstructor reconstructor x svs in
+--     (ctrs ++ " " ++ " :: " ++ adt)
+
+-- prettyConstructor :: ((D, Integer) -> (D, C)) -> Name -> [(Name, CV)] -> String
+-- prettyConstructor _ _ [] = ""
+-- prettyConstructor reconstructor prefix ((var, cv) : rest)
+--   | prefix `isPrefixOf` var =
+--     let var'     = fromMaybe "" $ stripPrefix prefix var   in
+--     let adt      = takeWhile (/= '$') (drop 1 var')        in
+--     let (_, ctr) = reconstructor (adt, getSelector cv)     in
+--     let fields   = prettyField reconstructor var rest      in
+--         (ctr ++ " " ++ braces (stripRedundant fields))
+--   | otherwise = prettyConstructor reconstructor prefix rest
+--   where
+--     braces = ("{" ++) . (++ "}")
+--     stripRedundant s
+--       | ", " `isSuffixOf` s = fromMaybe ""  $ stripSuffix ", " s
+--       | otherwise           = s
+--     stripSuffix a b = reverse <$> stripPrefix (reverse a) (reverse b)
+
+-- prettyField :: ((D, Integer) -> (D, C)) -> Name -> [(Name, CV)] -> String
+-- prettyField _ _ [] = ""
+-- prettyField reconstructor prefix ((var, cv) : rest)
+--   | (prefix ++ "$field") `isPrefixOf` var =
+--     let prefix' = (prefix ++ "$field")                  in
+--     let var'   = fromMaybe "" $ stripPrefix prefix' var in
+--     let num     = takeWhile (/= '$') var'               in
+--       if (prefix' ++ num) == var
+--          then getVal cv ++ ", " ++ prettyField reconstructor prefix rest
+--          else prettyConstructor reconstructor (prefix' ++ num) rest
+--               ++ ", " ++ prettyField reconstructor prefix rest
+--   | otherwise = prettyField reconstructor prefix rest
+
+-- getSelector :: CV -> Integer
+-- getSelector s = read (takeWhile (/= ' ') $ show s) :: Integer
+
+-- getVal :: CV -> String
+-- getVal cv = takeWhile (/= ' ') $ show cv
 
